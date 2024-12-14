bc629af8289bfbcf18d59a897f937391a0a8f38a
Moazzem-Bhuiyan <tiyonbhuiyan.201@gmail.com>
2024-12-14 11:50:21 +0600

    up

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,28 +1,35 @@
 const Footer = () => {
-  return (
-    <div className=" bg-main w-full md:h-[266px] md:flex justify-around items-center p-4 ">
-      <div className=" space-y-3 ">
-        <h1 className=" md:text-center text-2xl">ADDRESS</h1>
-        <li>unitedtags.x10.mx</li>
-        <li>New York, USA</li>
-        <li>Test 12345</li>
-      </div>
+     return (
+          <div className=" bg-main w-full md:h-[266px] md:flex justify-around items-center p-4 ">
+               <div className=" space-y-3 ">
+                    <h1 className=" md:text-center text-2xl">ADDRESS</h1>
+                    <li className=" text-center md:text-start ">unitedtags.x10.mx</li>
+                    <li className=" text-center md:text-start ">New York, USA</li>
+                    <li className=" text-center md:text-start ">Test 12345</li>
+               </div>
+               <hr />
 
-      <div className=" space-y-3">
-        <h1 className=" md:text-center text-2xl">CONTACT</h1>
-        <li>Get in touch!</li>
-        <li>+1-12345-678910</li>
-        <li>abc@gmail.com</li>        
-      </div>
+               <div className=" space-y-3">
+                    <h1 className="text-2xl">CONTACT</h1>
 
-      <div className=" space-y-3">
-        <h1 className=" md:text-center text-2xl">QUICK LINKS</h1>
-        <li>Shop now</li>
-        <li>Legal Notice</li>
-        <li>Privacy</li>
-      </div>
-    </div>
-  );
+                    <div className=" flex flex-col justify-center">
+                         <li className=" text-center md:text-start ">Get in touch!</li>
+                         <li className=" text-center md:text-start ">+1-12345-678910</li>
+                         <li className=" text-center md:text-start ">abc@gmail.com</li>
+                    </div>
+               </div>
+               <hr />
+
+               <div className=" space-y-3">
+                    <h1 className=" md:text-center text-center text-2xl">
+                         QUICK LINKS
+                    </h1>
+                    <li className=" text-center md:text-start">Shop now</li>
+                    <li className=" text-center md:text-start  ">Legal Notice</li>
+                    <li className=" text-center md:text-start  ">Privacy</li>
+               </div>
+          </div>
+     );
 };
 
 export default Footer;
